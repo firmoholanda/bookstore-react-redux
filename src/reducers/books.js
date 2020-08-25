d0551705281980d6002e891ef7ab52509dcd1a8b
firmo holanda <firmo@viize.com>
2020-08-25 17:20:11 -0300

    create reducers

---
+++ src/reducers/books.js
@@ -0,0 +1,13 @@
+import { ADD_BOOK, REMOVE_BOOK } from '../actions/actionTypes'
+
+const bookReducer = (state = [], action) => {
+  if (action.type === ADD_BOOK) {
+    return [...state, action.payload];
+  }
+  if (action.type === REMOVE_BOOK) {
+    return state.filter((book) => book.id !== action.payload.book.id);
+  }
+  return state;
+};
+
+export default bookReducer;
---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from 'redux';
+
+import bookReducer from './books';
+
+const rootReducer = combineReducers({
+  books: bookReducer,
+});
+
+export default rootReducer;
