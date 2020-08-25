df42f584a9f75a1b1afc354eb537e16528bd13c9
firmo holanda <firmo@viize.com>
2020-08-25 17:19:54 -0300

    create BooksForm and BooksList containers

---
+++ src/containers/BooksForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const BooksForm = () => {
+  const categories = ['action', 'biography', 'history', 'horror', 'kids', 'learning', 'sci-fi'];
+  return (
+    <form>
+      <input type="text" name="book" placeholder="new book" />
+      <select>
+        {categories.map((category) => (
+          <option key={category} value={category}>{category}</option>
+        ))}
+      </select>
+      <button type="submit">new book</button>
+    </form>
+  );
+};
+
+export default BooksForm;
---
+++ src/containers/BooksList.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import PropTypes from 'prop-types';
+import Book from '../components/Book';
+
+const BooksList = ({ books }) => (
+  <table>
+    <thead>
+      <tr>
+        <th>id:</th>
+        <th>title:</th>
+        <th>category:</th>
+      </tr>
+    </thead>
+    <tbody>
+      {books.map((book) => <Book book={book} key={book.id} />)}
+    </tbody>
+  </table>
+);
+
+const mapStateToProps = (state) => ({ books: state.books });
+
+BooksList.defaultProps = {
+  books: [],
+};
+
+BooksList.propTypes = {
+  books: PropTypes.arrayOf(PropTypes.object),
+};
+
+export default connect(mapStateToProps)(BooksList);
