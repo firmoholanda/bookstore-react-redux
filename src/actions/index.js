27dcdab83de1a6af00de7ddb54585f92e7838acd
firmo holanda <firmo@viize.com>
2020-08-27 16:52:57 -0300

    add payload to actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -3,10 +3,10 @@ import { ADD_BOOK, REMOVE_BOOK } from './actionTypes'
 
 // action creators
 
-export function addBook(text) {
-  return { type: ADD_BOOK, text }
+export function addBook(book) {
+  return { type: ADD_BOOK, payload: book }
 }
 
-export function removeBook(text) {
-  return { type: REMOVE_BOOK, text }
+export function removeBook(book) {
+  return { type: REMOVE_BOOK, payload: book }
 }
