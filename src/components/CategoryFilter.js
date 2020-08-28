763b1272073038bd5836392894c32fcf9da4676b
firmo holanda <firmo@viize.com>
2020-08-28 16:35:43 -0300

    create category filter

---
+++ src/components/CategoryFilter.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const CategoryFilter = ({ filterCategory }) => {
+  const handleFilterChange = (event) => {
+    const { value } = event.target;
+    filterCategory(value);
+  };
+
+  const categories = ['action', 'biography', 'history', 'horror', 'kids', 'learning', 'sci-fi'];
+
+  return (
+    <select name="category" onChange={handleFilterChange}>
+      <option value="all">all</option>
+      {categories.map((category) => (
+        <option key={category} value={category}>{category}</option>
+      ))}
+    </select>
+  );
+};
+
+CategoryFilter.propTypes = {
+  filterCategory: PropTypes.func.isRequired,
+};
+
+export default CategoryFilter;
